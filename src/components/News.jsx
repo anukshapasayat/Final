9220653a23cae6dc8e16940a5a53bcd69e146406
SaquibAfzal <1906202@kiit.ac.in>
2023-03-14 23:06:52 +0530

    Line Chart API request error

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -10,7 +10,7 @@ const {Text,Title} = Typography;
 const { Option } = Select;
 const demoImage = 'https://www.bing.com/th?id=OVFT.mpzuVZnv8dwIMRfQGPbOPC&pid=News'
 
-const News = (simplified) => {
+const News = ({ simplified }) => {
   const [newsCategory, setNewsCategory] = useState('Cryptocurrency');
   const { data } = useGetCryptosQuery(100);
   const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory, count: simplified ? 6 : 12 });
--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,27 +1,36 @@
-import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const cryptoApiHeaders={
-    'X-RapidAPI-Key': '5dbda506dbmshd30f126ac8e6070p1cbac0jsn160697eb96c6',
-   'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
-}
+const cryptoApiHeaders = {
+  "X-RapidAPI-Key": "5dbda506dbmshd30f126ac8e6070p1cbac0jsn160697eb96c6",
+  "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
+};
 
-const baseUrl= 'https://coinranking1.p.rapidapi.com';
+const baseUrl = "https://coinranking1.p.rapidapi.com";
 
-const createRequest=(url)=>({url,headers:cryptoApiHeaders})
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
 
-export const cryptoApi= createApi({
-    reducerPath: 'cryptoApi',
-    baseQuery:fetchBaseQuery({baseUrl}),
-    endpoints:(builder)=>({
-        getCryptos: builder.query({
-            query:(count) => createRequest(`/coins?limit=${count}`),
-        })
-    })
+export const cryptoApi = createApi({
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: (count) => createRequest(`/coins?limit=${count}`),
+    }),
+    getCryptoDetails: builder.query({
+      query: (coinId) => createRequest(`/coin/${coinId}`),
+    }),
+    getCryptoHistory: builder.query({
+      query: ( {coinId, timeperiod}) =>
+        createRequest(`coin/${coinId}/history?timeperiod=${timeperiod}`),
+    }),
+  }),
 });
 
-export const{
-    useGetCryptosQuery,
-}= cryptoApi;
+export const {
+  useGetCryptosQuery,
+  useGetCryptoDetailsQuery,
+  useGetCryptoHistoryQuery,
+} = cryptoApi;
 
 // const options = {
 //     method: 'GET',
