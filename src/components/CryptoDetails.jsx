45f51a7dca1446625b0c2e045f3f9070d35ff40c
SaquibAfzal <1906202@kiit.ac.in>
2023-03-14 23:30:56 +0530

    Line Chart API request error-1

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -28,7 +28,7 @@ const CryptoDetails = () => {
   const { coinId } = useParams();
   const [timeperiod, setTimeperiod] = useState("7d");
   const { data, isFetching } = useGetCryptoDetailsQuery(coinId);
-  const { data: coinHistory } = useGetCryptoHistoryQuery(coinId, timeperiod);
+  const { data: coinHistory } = useGetCryptoHistoryQuery({coinId, timeperiod});
   const cryptoDetails = data?.data?.coin;
   console.log(data);
   console.log(coinHistory);
